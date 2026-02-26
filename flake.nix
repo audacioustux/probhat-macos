b0b326045b862c298105d54396fa485de52c5234
Tanjim H. <contact@audacioustux.com>
2026-02-26 21:15:30 +0600

    refactor(flake): use idiomatic structure and add formatter

--- flake.nix
+++ flake.nix
@@ -1,61 +1,69 @@
 {
   description = "Probhat Bengali keyboard layout for macOS";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
   outputs = { self, nixpkgs }:
     let
-      systems = [ "aarch64-darwin" "x86_64-darwin" ];
-      mkProbhat = pkgs: pkgs.stdenvNoCC.mkDerivation {
-        pname = "probhat-keylayout";
-        version = "1.0.0";
-        src = ./.;
-        dontBuild = true;
-        installPhase = ''
-          mkdir -p "$out/Library/Keyboard Layouts"
-          cp Probhat.keylayout Probhat.icns "$out/Library/Keyboard Layouts/"
-        '';
-        meta = {
-          description = "Probhat fixed-layout Bengali keyboard for macOS";
-          homepage = "https://github.com/mdminhazulhaque/probhat-macos";
-          license = nixpkgs.lib.licenses.mit;
-          platforms = nixpkgs.lib.platforms.darwin;
-        };
-      };
+      supportedSystems = [ "aarch64-darwin" "x86_64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      packages = nixpkgs.lib.genAttrs systems (system:
-        let pkg = mkProbhat nixpkgs.legacyPackages.${system};
-        in { probhat = pkg; default = pkg; });
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          probhat = pkgs.stdenvNoCC.mkDerivation {
+            pname = "probhat-keylayout";
+            version = "1.0.0";
+            src = ./.;
 
-      # Usage in nix-darwin:
-      #   inputs.probhat-macos.url = "github:audacioustux/probhat-macos";
-      #   imports = [ probhat-macos.darwinModules.default ];
-      #   programs.probhat.enable = true;
+            dontBuild = true;
+
+            installPhase = ''
+              mkdir -p "$out/Library/Keyboard Layouts"
+              cp Probhat.keylayout Probhat.icns "$out/Library/Keyboard Layouts/"
+            '';
+
+            meta = {
+              description = "Probhat fixed-layout Bengali keyboard for macOS";
+              homepage = "https://github.com/audacioustux/probhat-macos";
+              license = nixpkgs.lib.licenses.mit;
+              platforms = nixpkgs.lib.platforms.darwin;
+            };
+          };
+          default = self.packages.${system}.probhat;
+        });
+
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+
       darwinModules.default = { config, lib, pkgs, ... }:
         let
+          cfg = config.programs.probhat;
           dst = "/Library/Keyboard Layouts";
-          pkg = mkProbhat pkgs;
+          pkg = self.packages.${pkgs.system}.default;
           src = "${pkg}/Library/Keyboard Layouts";
-        in {
+        in
+        {
           options.programs.probhat.enable =
             lib.mkEnableOption "Probhat Bengali keyboard layout";
 
-          config = lib.mkMerge [
-            (lib.mkIf config.programs.probhat.enable {
-              system.activationScripts.postActivation.text = ''
+          config = lib.mkIf (pkgs.stdenv.isDarwin) {
+            system.activationScripts.postActivation.text =
+              if cfg.enable then ''
                 echo "Installing Probhat keyboard layout..." >&2
                 mkdir -p "${dst}"
                 install -m 644 "${src}/Probhat.keylayout" "${dst}/Probhat.keylayout"
                 install -m 644 "${src}/Probhat.icns" "${dst}/Probhat.icns"
-              '';
-            })
-            (lib.mkIf (!config.programs.probhat.enable) {
-              system.activationScripts.postActivation.text = ''
+              '' else ''
                 echo "Removing Probhat keyboard layout..." >&2
                 rm -f "${dst}/Probhat.keylayout"
                 rm -f "${dst}/Probhat.icns"
               '';
-            })
-          ];
+          };
         };
     };
 }
